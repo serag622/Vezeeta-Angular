93c500084fc4582587ddef83f78fd8fd282ab453
mohamed salah abdellatif <m.salah1595@gmail.com>
2021-11-07 18:12:57 +0200

    Doctor Page

---
+++ src/app/Body/doctor-information/doctor-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-information',
+  templateUrl: './doctor-information.component.html',
+  styleUrls: ['./doctor-information.component.css']
+})
+export class DoctorInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
