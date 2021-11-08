c33bee1a014d0fe5081ac6d93a7043d95997e33d
Aya Mamdouh <ayammdouh17@gmail.com>
2021-11-08 21:00:10 +0200

    create contact us componant , search bar componant

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './Compontents/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"contactUS",component:ContactUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './Layout/nav-bar/nav-bar.component';
 import { FooterComponent } from './Layout/footer/footer.component';
+import { ContactUsComponent } from './Compontents/contact-us/contact-us.component';
+import { SearchBarComponent } from './shared/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    ContactUsComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
