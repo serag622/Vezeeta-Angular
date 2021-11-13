0b274433eb1a86fc1d7c83dc7b78caa10d49ea2f
serag622 <serag622@gmail.com>
2021-11-13 22:06:50 +0200

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './Layout/nav-bar/nav-bar.component';
 import { FooterComponent } from './Layout/footer/footer.component';
+import { HomeComponent } from './HomePage/home/home.component';
+import { HomeHeaderComponent } from './HomePage/home-header/home-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    HomeHeaderComponent
   ],
   imports: [
     BrowserModule,
